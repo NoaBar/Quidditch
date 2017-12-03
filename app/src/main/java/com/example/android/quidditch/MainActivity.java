f75db840c79ac5c0f7eb6a7b7405182911f5c7a0
Noah Bar <noahbar@gmail.com>
2017-12-03 12:00:39 +0100

    initial commit

---
+++ app/src/main/java/com/example/android/quidditch/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.android.quidditch;
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    /** Griffindor */
+
+    int scoreG = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void add10ForG (View view) {
+        scoreG = scoreG + 10;
+        displayForG (scoreG);
+
+    }
+
+    public void add20ForG (View view) {
+        scoreG = scoreG + 20;
+        displayForG (scoreG);
+    }
+
+    public void add150ForG (View view) {
+        scoreG = scoreG + 150;
+        displayForG (scoreG);
+    }
+
+    /**
+     * Displays the given score for G.
+     */
+    public void displayForG(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.G_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+
+/** Slytherin */
+
+    int scoreS = 0;
+
+    public void add10ForS (View view) {
+        scoreS = scoreS + 10;
+        displayForS (scoreS);
+
+    }
+
+    public void add20ForS (View view) {
+        scoreS = scoreS + 20;
+        displayForS (scoreS);
+    }
+
+    public void add150ForS (View view) {
+        scoreS = scoreS + 150;
+        displayForS (scoreS);
+    }
+
+    /**
+     * Displays the given score for S.
+     */
+    public void displayForS(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.S_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void resetScore (View view) {
+        scoreS = 0;
+        displayForS (scoreS);
+        scoreG = 0;
+        displayForG (scoreG);
+    }
+
+}
